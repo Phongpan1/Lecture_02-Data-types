f0ddb6e5b38baa8e444aae0ee8b16b898b684631
Phongpan1 <167884003+Phongpan1@users.noreply.github.com>
2024-05-04 22:22:27 +0700

    Add files via upload

---
+++ if-ternary.js
@@ -0,0 +1,4 @@
+const status = 300;
+const message = (status === 400) ? 'OK!' : 'Error!'; 
+
+console.log(message);
---
+++ test.js
@@ -0,0 +1,6 @@
+const name = 'christopher';
+if (name == 'christopher') {
+    console.log('the values are the same');
+} else {
+    console.log('the values NOT are the same');
+}
